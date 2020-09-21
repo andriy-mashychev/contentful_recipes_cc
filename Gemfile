fe89718f498b99dbc0d2159eb3398cbf8da64765
Andriy Mashychev <andriy.mashychev@live.com>
2020-09-21 08:34:06 +0200

    Record HTTP interactions in order to create a test suite.

--- Gemfile
+++ Gemfile
@@ -39,8 +39,10 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem "vcr", "~> 6.0"
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem "webmock", "~> 3.9"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -7,4 +9,20 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
+end
+
+require 'vcr'
+
+VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = false
+  config.cassette_library_dir = File.expand_path('fixtures/vcr_cassettes', __dir__)
+  config.hook_into :webmock
+  config.ignore_request { ENV['DISABLE_VCR'] }
+  config.ignore_localhost = true
+  config.default_cassette_options = {
+    record: :new_episodes
+  }
+  config.filter_sensitive_data('<CONTENTFUL_AT>') { ENV['CONTENTFUL_ACCESS_TOKEN'] }
+  config.filter_sensitive_data('<CONTENTFUL_SI>') { ENV['CONTENTFUL_SPACE_ID'] }
+  config.filter_sensitive_data('<CONTENTFUL_EI>') { ENV['CONTENTFUL_ENV_ID'] }
 end
